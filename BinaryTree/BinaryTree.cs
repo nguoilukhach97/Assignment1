7e490fb8d585c49782bb9a88a5a403bc029aeb44
hungth230297@gmail.com <hungth230297@gmail.com>
2020-06-29 14:11:06 +0700

    complete

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class BinaryTree
+    {
+        public NodeTree root { get; set; }
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void InsertNode(int data)
+        {
+            if (root == null)
+                root = new NodeTree(data);
+            else
+                root.Insert(data);
+        }
+        public void PreOrder(NodeTree node)
+        {
+            if (node == null) return;
+            Console.Write(node.Data +" ");
+            PreOrder(node.Left);
+            PreOrder(node.Right);
+        }
+        public void InOrder(NodeTree node)
+        {
+            if (node == null) return;
+            InOrder(node.Left);
+            Console.Write(node.Data+" ");
+            InOrder(node.Right);
+        }
+        public void PostOrder(NodeTree node)
+        {
+            if (node == null) return;
+            PreOrder(node.Left);
+            PreOrder(node.Right);
+            Console.Write(node.Data + " ");
+        }
+        public bool Search(NodeTree node, int x)
+        {
+            if (node == null) return false;
+            if (node.Data == x) return true;
+            return x > node.Data ? Search(node.Right, x) : Search(node.Left, x);
+            
+        }
+    }
+}
---
+++ BinaryTree/NodeTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class NodeTree
+    {
+        public int Data { get; set; }
+        public NodeTree Left { get; set; }
+        public NodeTree Right { get; set; }
+        public NodeTree(int data)
+        {
+            this.Data = data;
+            Left = Right = null;
+        }
+        public void Insert(int data)
+        {
+            if(data <Data)
+            {
+                if (Left == null) 
+                    Left = new NodeTree(data);
+                else
+                    Left.Insert(data);
+            }
+            if (data > Data)
+            {
+                if (Right == null)
+                    Right = new NodeTree(data);
+                else
+                    Right.Insert(data);
+            }
+        }
+    }
+}
